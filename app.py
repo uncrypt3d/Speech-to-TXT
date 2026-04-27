835d6f127509708bd780562260d57ed4a6314769
ay0 who d1s <86315452+uncrypt3d@users.noreply.github.com>
2026-04-27 16:55:35 +0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,18 @@
+from faster_whisper import WhisperModel
+
+model = WhisperModel("large", device="cpu", compute_type="int8")
+
+segments, info = model.transcribe(
+    "teksti.mp3",
+    language="fi"
+)
+
+print("\n--- LITTEROITU TEKSTI ---\n")
+
+full_text = ""
+for segment in segments:
+    print(segment.text)
+    full_text += segment.text + " "
+
+with open("litterointi.txt", "w", encoding="utf-8") as f:
+    f.write(full_text)
